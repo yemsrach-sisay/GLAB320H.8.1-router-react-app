a18c8fd174c9dedb94f6876b4cdbd84519cba5e5
yemsrach-sisay <97808898+yemsrach-sisay@users.noreply.github.com>
2024-07-17 11:29:08 -0400

    i created route react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import Currencies from "../src/pages/Currencies ";
+import Main from "../src/pages/Main ";
+import Price from "../src/pages/Price ";
+import Nav from "../src/components/Nav";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/currencies" element={<Currencies />} />
+        <Route path="/price" element={<Price />} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
